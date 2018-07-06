926743f76d10c31c0cf080a900e0776dc0a7d3c3
ferrebenoit <ferrebenoit@users.noreply.github.com>
2018-07-06 23:09:18 +0200

    update bash expect implementation

--- switchhandler/device/protocol/expect/unix/unix_expect.py
+++ switchhandler/device/protocol/expect/unix/unix_expect.py
@@ -4,6 +4,7 @@ Created on 19 janv. 2018
 @author: ferre
 '''
 from switchhandler.device.protocol.expect.device_expect import DeviceExpect
+from switchhandler.device.device_exception import CommandSyntaxErrorException
 
 
 class UnixExpect(DeviceExpect):
@@ -16,3 +17,12 @@ class UnixExpect(DeviceExpect):
         Constructor
         '''
         super().__init__('unix', IP, vendor, site, dryrun)
+
+    def execute(self, command_name, *args, **kwargs):
+        try:
+            return super().execute(command_name, *args, **kwargs)
+        except CommandSyntaxErrorException:
+            self.log_critical("Script will terminate changes are not written")
+            self.log_debug(self.before())
+            self.log_debug(self.after())
+            quit()
--- switchhandler/device/protocol/expect/unix/vendor/bash/bash_expect.py
+++ switchhandler/device/protocol/expect/unix/vendor/bash/bash_expect.py
@@ -4,8 +4,14 @@ Created on 19 janv. 2018
 @author: ferre
 '''
 from switchhandler.device.protocol.expect.unix.unix_expect import UnixExpect
+from switchhandler.utils.decorator.class_register import get_registered_classes,\
+    registered_class_scan, registered_class
+from switchhandler.device.protocol.expect.unix.vendor.bash import CATEGORY_BASH
+from switchhandler.device import CATEGORY_DEVICE_EXPECT
 
 
+@registered_class_scan(BasePackage='switchhandler.device.protocol.expect.unix.vendor.bash')
+@registered_class(category=CATEGORY_DEVICE_EXPECT, registered_name='bash')
 class BashExpect(UnixExpect):
     '''
     classdocs
@@ -16,3 +22,6 @@ class BashExpect(UnixExpect):
         Constructor
         '''
         super().__init__('bash', IP, vendor, site, dryrun)
+
+    def getCommands(self):
+        return get_registered_classes(CATEGORY_BASH)
